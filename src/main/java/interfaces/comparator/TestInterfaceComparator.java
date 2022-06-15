498cdcb7f4608149b4a9735a076d8f701cbd1b2e
Miguel Ángel <magcarnota@gmail.com>
2022-06-15 13:12:34 +0200

    Ampliación interfaz Comparator.

--- src/main/java/interfaces/comparator/TestInterfaceComparator.java
+++ src/main/java/interfaces/comparator/TestInterfaceComparator.java
@@ -5,6 +5,13 @@ import java.util.Comparator;
 
 /**
  * Clase para testing de interfaz Comparator<T>
+ * Cuenta con los métodos por defecto:
+ * thenComparing(<otro comparador>) para una vez comparados por el anterior, hacerlo por el indicado
+ * reversed() para invertir el orden de ordenación
+ * Cuenta con los métodos estáticos:
+ * nullsFirst(<comparador>) para devolver un comparador que muestre los nulos al principio
+ * nullsLast(<comparador>)  para devolver un comparador que muestre los nulos al final
+ *
  * @author magcarnota
  */
 public class TestInterfaceComparator {
@@ -27,12 +34,42 @@ public class TestInterfaceComparator {
         Arrays.sort(array, new MiClaseComparatorEdad());
 
         mostrarArray(array);
+
+        System.out.println("Usamos el método por defecto thenComparing(otro comparador) del interfaz Comparator");
+        System.out.println("para que ordene primero por edad y después por nombre:");
+        MiClaseComparatorNombre comparadorPorNombre = new MiClaseComparatorNombre();
+        MiClaseComparatorEdad comparadorPorEdad = new MiClaseComparatorEdad();
+        Arrays.sort(array, comparadorPorEdad.thenComparing(comparadorPorNombre));
+
+        mostrarArray(array);
+
+        System.out.println("Usamos el método por defecto reversed(otro comparador) del interfaz Comparator");
+        System.out.println("para que ordene por nombre del revés:");
+        Arrays.sort(array, comparadorPorNombre.reversed());
+
+        mostrarArray(array);
+
+        array = Arrays.copyOf(array, array.length + 1);
+        array[3] = null;
+
+        System.out.println("Con el método estático nullsFirst(<comparador>) obtenemos un comparador que");
+        System.out.println("tenga en cuenta los valores null y los muestre al principio:");
+        Arrays.sort(array, Comparator.nullsFirst(comparadorPorNombre));
+
+        mostrarArray(array);
+
+        System.out.println("Con el método estático nullsLast(<comparador>) obtenemos un comparador que");
+        System.out.println("tenga en cuenta los valores null y los muestre al final:");
+        Arrays.sort(array, Comparator.nullsLast(comparadorPorNombre));
+
+        mostrarArray(array);
     }
 
     static void mostrarArray(MiClase[] array) {
         for(MiClase objeto : array) {
             System.out.println(objeto);
         }
+        System.out.println();
     }
 }
 
